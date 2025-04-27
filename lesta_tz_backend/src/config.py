e57994708916f097aefd5c23d46d1c0ee8aab112
Gennadiy Kharatyan <83674229+genacr0co@users.noreply.github.com>
2025-04-27 16:25:10 +0500

    fix docker compose

--- lesta_tz_backend/src/config.py
+++ lesta_tz_backend/src/config.py
@@ -1,5 +1,7 @@
-DB_HOST = 'localhost'
-DB_PORT = '5432'
-DB_NAME = 'tf_idf'
-DB_USER = 'postgres'
-DB_PASS = 'postgres'
+import os
+
+DB_HOST = os.getenv('DB_HOST', 'localhost')  # Если переменная не задана, используем 'localhost'
+DB_PORT = os.getenv('DB_PORT', '5432')  # По умолчанию порт 5432, если не указано
+DB_NAME = os.getenv('DB_NAME', 'tf_idf')
+DB_USER = os.getenv('DB_USER', 'postgres')
+DB_PASS = os.getenv('DB_PASS', 'postgres')
