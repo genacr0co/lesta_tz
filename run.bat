60ff96d361650e1dea0aeb8d92ee02232a491375
Gennadiy Kharatyan <83674229+genacr0co@users.noreply.github.com>
2025-05-02 08:10:25 +0500

    Fix Bugs

--- run.bat
+++ run.bat
@@ -1,9 +1,8 @@
 @echo off
-@REM echo Stopping old containers...
 
 @REM docker-compose -f ./lesta_tz_backend/docker-compose.yml down
 
-echo Starting containers...
+echo Starting Backend containers...
 
 docker-compose -f ./lesta_tz_backend/docker-compose.yml up --build -d
 
@@ -12,5 +11,20 @@ if %errorlevel% neq 0 (
     pause
 )
 
-@REM echo Project successfully started at http://localhost:8000
+echo Backend successfully started at http://localhost:8001
+
+echo Starting Frontend containers...
+
+docker-compose -f ./lesta_tz_frontend/docker-compose.yml up --build -d
+
+if %errorlevel% neq 0 (
+    echo Error during installation! Exit code: %errorlevel%
+    pause
+)
+
+echo Frontend successfully started at http://localhost:3001
+
+echo Backend successfully started at http://localhost:8001
+
+
 pause
