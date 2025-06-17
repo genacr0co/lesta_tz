1d92744ee4dd078505bd8fb52b19bedaa67e0b7f
Gennadiy Kharatyan <83674229+genacr0co@users.noreply.github.com>
2025-06-17 12:21:56 +0500

    fix bugs

--- lesta_tz_frontend/src/widgets/RegisterForm/components/RegisterForm.tsx
+++ lesta_tz_frontend/src/widgets/RegisterForm/components/RegisterForm.tsx
@@ -1,10 +1,9 @@
 import { useState} from "react";
 import {createTrigger } from "trigger-man";
 import { useRouter } from 'next/router'
-import { Spin } from 'antd';
+import { Spin, Checkbox, Modal  } from 'antd';
 import { LoadingOutlined } from '@ant-design/icons';
 import { AuthFooter } from "@/features";
-
 import { TRIGGER } from "@/shared/const";
 
 import { Props } from "../lib/props";
@@ -14,11 +13,22 @@ import { postRegister} from "../api/services";
 export const RegisterForm = (props: Props) => {
     const router = useRouter()
     const [loading, setLoading] = useState<boolean>(false);
+    const [isChecked, setIsChecked] = useState<boolean>(false);
     const [error, setError] = useState<boolean>(false);
     const [name, setName] = useState<string>();
     const [email, setEmail] = useState<string>();
     const [password, setPassword] = useState<string>();
     const [password2, setPassword2] = useState<string>();
+
+      const [isModalOpen, setIsModalOpen] = useState(false);
+
+        const showModal = () => {
+            setIsModalOpen(true);
+        };
+
+        const handleCancel = () => {
+            setIsModalOpen(false);
+        };
 
     const request = () => {
 
@@ -96,19 +106,30 @@ export const RegisterForm = (props: Props) => {
                 />
             </div>
 
-               <div className={styles.FormInput}>
-                <div className={styles.FormInputTitle}>Repeat password</div>
-                <input
-                    type="password"
-                    name={password2}
-                    onChange={(e) => setPassword2(e.target.value)}
-                    placeholder="Enter your password"
-                    className={styles.FormInputBody}
-                    required
-                />
+            <div className={styles.FormInput}>
+            <div className={styles.FormInputTitle}>Repeat password</div>
+            <input
+                type="password"
+                name={password2}
+                onChange={(e) => setPassword2(e.target.value)}
+                placeholder="Enter your password"
+                className={styles.FormInputBody}
+                required
+            />
             </div>
 
-            <button type="submit" disabled={loading} className={styles.FormButton}>
+            <div  className={styles.FormCheckbox}>
+                <Checkbox 
+                onChange={() => {
+                    setIsChecked(!isChecked)
+                }}
+                >
+                   I agree to the <span className={styles.link} onClick={showModal}>Terms</span> and <span className={styles.link} onClick={showModal}>Privacy Policy</span>
+                </Checkbox>
+              
+            </div>
+
+            <button type="submit" disabled={loading || !isChecked} className={`${!isChecked && styles.disableButton} ${styles.FormButton}`}>
                 {loading ? 
                    <Spin indicator={<LoadingOutlined spin />} />
                 :
@@ -132,6 +153,29 @@ export const RegisterForm = (props: Props) => {
                </div>
             </div>
             <AuthFooter/>
+
+            <Modal
+            title="Terms and Privacy Policy"
+            closable={{ 'aria-label': 'Custom Close Button' }}
+            open={isModalOpen}
+            onCancel={handleCancel}
+            footer={null}
+            >
+                <p>Создавая аккаунт, вы соглашаетесь с тем, что:</p>
+                <br/>
+                <p>
+                    <ul>
+                    <li>Ваша душа теперь принадлежит нам (обратно не возвращается).</li>
+                    <li>Мы имеем право присылать вам мемы в 3 часа ночи.</li>
+                    <li>Вы обязуетесь участвовать в танцевальных баттлах по первому зову.</li>
+                    <li>Ваш первенец будет назван в честь нашего сервиса.</li>
+                    <li>Печеньки (cookies) — не съедобные. Смиритесь.</li>
+                    <li>Ну и, в целом, вы становитесь нашим верным подданным навеки вечные.</li>
+                    </ul>
+                </p>
+                   <br/>
+                <p><em>*Это не является юридическим документом. Всё выше сказанное — лишь шутка 😄*</em></p>
+            </Modal>
         </>
     );
 };
